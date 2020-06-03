3714a863c849fe97292b560bd15c7f46c93f9fc0
Brent Groves <brent.groves@gmail.com>
2020-06-03 09:39:00 -0400

    Added/Update source code

--- src/reducers/Global.js
+++ src/reducers/Global.js
@@ -2,34 +2,14 @@ import * as types from '../constants/ActionTypes'
 import * as errorType from '../constants/ErrorType'
 import * as errorSeverity from '../constants/ErrorSeverity'
 import { log } from "../utils/log";
-import { firstOfWeek, endOfWeek } from "../utils/dates";
+import { FirstDayWeek,LastDayWeek,FirstDayMonth,LastDayMonth} from "../utils/dates";
 
-//dt = new Date(); 
-
-//console.log(endOfWeek(dt).toString());
-
-
-//var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
-//var last = curr.getDate() - curr.getDay() + 6; 
-//var last = date.getDate() - (date.getDay() - 1) + 6;
-//var lastday = date.getDate() - (date.getDay() - 1) + 6;
-// var last = first + 6; // last day is the first day + 6
-//var firstDayOfWeek = new Date(curr.setDate(first)).toString();
-// var lastDayOfWeek= new Date(curr.setDate(last)).toString();
-var firstDayOfWeek = firstOfWeek(new Date());
-var lastDayOfWeek = endOfWeek(new Date());
-var curr = new Date(); // get current date
-// This section is wrong. Replace with a function.
-var firstDayOfMonth = new Date(curr.getFullYear(), curr.getMonth(), 1);
-var lastDayOfMonth = new Date(curr.getFullYear(), curr.getMonth() + 1, 0);
-log(`cur: ${curr},firstDayOfWeek:${firstDayOfWeek},lastDayOfWeek:${lastDayOfWeek}`);
-function addDays(theDate, days) {
-    return new Date(theDate.getTime() + days*24*60*60*1000);
-}
-
-lastDayOfMonth = addDays(lastDayOfMonth, 1);
-// last millisecond of previous date
-lastDayOfMonth = new Date(lastDayOfMonth.getTime() - 1);
+var today = new Date();
+var firstDayOfWeek = FirstDayWeek(today);
+var lastDayOfWeek = LastDayWeek(today);
+var firstDayOfMonth = FirstDayMonth(today);
+var lastDayOfMonth = LastDayMonth(today);
+log(`today: ${today},firstDayOfWeek:${firstDayOfWeek},lastDayOfWeek:${lastDayOfWeek},firstDayOfMonth:${firstDayOfMonth},lastDayOfMonth:${lastDayOfMonth}`);
 
 const initState = {
   submitting: false,
--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,21 +1,47 @@
-function endOfWeek(date)
+function LastDayWeek(date)
   {
-     
-    var lastday = date.getDate() - date.getDay() + 6;
-    return new Date(date.setDate(lastday));
+    var newDate = new Date(date.getTime());  // Duplicate date
+    var lastDayTS = newDate.getDate() - newDate.getDay() + 6;
+    var lastDay = new Date(newDate.setDate(lastDayTS));
+    lastDay.setHours(23,59,59,0);
+    return lastDay;
  
   }
 
-  function firstOfWeek(date)
+  function FirstDayWeek(date)
   {
-    var firstDay = date.getDate() - date.getDay(); // First day is the day of the month - the day of the week     
-    return new Date(date.setDate(firstDay));
+    var newDate = new Date(date.getTime());  // Duplicate date
+    var firstDayTS = newDate.getDate() - newDate.getDay();
+    var firstDay = new Date(newDate.setDate(firstDayTS));
+    firstDay.setHours(0,0,0,0);
+    return firstDay;
  
+  }
+
+  function FirstDayMonth(date)
+  {
+    var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    firstDay.setHours(0,0,0,0);
+    return firstDay;
+  }
+
+  function LastDayMonth(date)
+  {
+    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    lastDay.setHours(23,59,59,0);
+    return lastDay;
+  }
+
+  function addDays(theDate, days) {
+    return new Date(theDate.getTime() + days*24*60*60*1000);
   }
 
 
 
 module.exports = {
-    firstOfWeek,
-    endOfWeek
+    FirstDayWeek,
+    LastDayWeek,
+    FirstDayMonth,
+    LastDayMonth
 }
+
